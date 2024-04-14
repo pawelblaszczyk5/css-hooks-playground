845b0e64fdb3a0df4b7744f6976c4833ed047491
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-04-14 20:10:00 +0200

    Some experiments with custom resolvers and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { createHooks } from "@css-hooks/react";
-
-const { styleSheet, css } = createHooks({
-  hooks: {
-    "&:active": "&:active",
-  },
-  debug: import.meta.env.DEV,
-});
+import { styleSheet, css } from "./css";
 
 const resolvedStyleSheet = styleSheet();
 
 export const App = () => (
   <>
     <style dangerouslySetInnerHTML={{ __html: resolvedStyleSheet }}></style>
-    <h1 style={css({ display: "flex" })}>Hello world</h1>
+    <h1
+      style={css({
+        display: "flex",
+        gap: "18px",
+        on: ($) => [$("hover", { backgroundColor: "red" })],
+      })}
+    >
+      Hello world <span>t</span>
+    </h1>
   </>
 );
